23b5339f410e408a34cee45a33c82a78c778d554
Aniket Yadav <anikety7905@gmail.com>
2025-09-17 23:38:32 +0530

    Update and rename Input to Input.py

---
+++ Input.py
@@ -0,0 +1,29 @@
+Input in Python
+
+name=input("enter your name:")
+print("Welcome",name)
+
+
+name=input("enter your age:")
+print("Your age is:",name)
+
+value=input("enter your value:")
+print(type(value))
+
+value=int(input("enter your value:"))
+print(type(value))
+
+value=float(input("enter your value:"))
+print(type(value))
+
+name=input("enter your name:")
+age=int(input("enter your age:"))
+marks=float(input("enter your marks:"))
+
+print("Welcome",name)
+print("age is :",age)
+print("your marks:",marks)
+print(type(name))
+print(type(age))
+print(type(marks))
+
