486b8fbfed4f1291dc8c8c5f662e472000208c72
Aniket Yadav <anikety7905@gmail.com>
2025-09-17 22:46:20 +0530

    Update Type Conversion.py

--- Type Conversion.py
+++ Type Conversion.py
@@ -1,1 +1,10 @@
+#Type Conversion
+#Implicit Type Conversion
+
+a=5
+b=2.0
+c=a+b
+print("The value of c is:",c)# c is float because b is float, so a is converted to float, a = 5.0 and b = 2.0
+print("The type of a is:",type(a)) # type of a is int
+print("The type of c is:",type(c)) # type of c is float
 
