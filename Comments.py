3a2e3fe6dc4657d967014dd57a933472d24b5629
Aniket Yadav <anikety7905@gmail.com>
2025-09-17 22:36:07 +0530

    Create Comments.py

---
+++ Comments.py
@@ -0,0 +1,15 @@
+# comment
+# this is a single line comment
+'''
+this is a multi line comment
+line 1  
+line 2
+line 3
+line 4
+'''
+"""this is also a multi line comment
+line 1
+line 2
+line 3
+line 4
+"""
