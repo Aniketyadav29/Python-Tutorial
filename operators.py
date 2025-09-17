ce1ac40d42a9820dfa2a7de248ddf4e468d23e75
Aniket Yadav <anikety7905@gmail.com>
2025-09-17 22:38:26 +0530

    Update operators.py

--- operators.py
+++ operators.py
@@ -23,3 +23,34 @@ print("the value of a<=b is:",a<=b)  # less than or equal to operator, 'False' b
 print("the value of a>b is:",a>b) # greater than operator, 'True' because 50 is greater than 20
 
 print("the value of a<b is:",a<b)   # less than operator, 'False' because 50 is not less than 20
+# Assignment Operators
+num=5
+num+=5
+print("num is:",num)
+
+num=5
+num-=5
+print("num is:",num)
+num=5
+num*=5
+print("num is:",num)
+
+num=5
+num/=5
+print("num is:",num)
+
+num=5
+num%=5
+print("num is:",num)
+
+num=5
+num **= 5
+print("num is:",num)
+
+# Logical Operators
+a=True
+b=False
+print("The value of a and b is:",a and b) # and operator, 'False' because both a and b are not true
+print("The value of a or b is:",a or b) # or operator, 'True' because at least one of a or b is true
+print("The value of not a is:",not a) # not operator, 'False'
+print("The value of not b is:",not b) # not operator, 'True'
