1ff2e311e1c54033791bed9477536370343c0c85
Aniket Yadav <anikety7905@gmail.com>
2025-09-17 22:37:36 +0530

    Update operators.py

--- operators.py
+++ operators.py
@@ -1,1 +1,25 @@
+# Operators in Python
+# Arithmetic Operators
+a=23
+b=34
+print("The value of a+b is",a+b)
+print("The value of a-b is",a-b)
+print("The value of a*b is",a*b)
+print("The value of a/b is",a/b)
+print("The value of a%b is",a%b)
+print("The value of a//b is",a//b)
 
+# Relational Operators
+a=50
+b=20
+print("The value of a==b is",a==b) # equality operator ,'False' because 50 is not equal to 20
+
+print("the value of a!=b is:",a!=b) # not equal to operator, 'True' because 50 is not equal to 20
+
+print("the value of a>=b is:",a>=b) # greater than or equal to operator, 'True' because 50 is greater than 20
+
+print("the value of a<=b is:",a<=b)  # less than or equal to operator, 'False' because 50 is not less than 20
+
+print("the value of a>b is:",a>b) # greater than operator, 'True' because 50 is greater than 20
+
+print("the value of a<b is:",a<b)   # less than operator, 'False' because 50 is not less than 20
