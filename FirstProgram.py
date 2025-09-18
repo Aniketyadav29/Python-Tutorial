c3269fcc969b610f3c8ac9d6910d49d81176626d
Aniket Yadav <anikety7905@gmail.com>
2025-09-18 09:39:58 +0530

    Update FirstProgram.py

--- FirstProgram.py
+++ FirstProgram.py
@@ -129,5 +129,37 @@ c=a+b
 print("The value of c is:",c) # c is float because b is float, so a is converted to float, a = 5.0 and b = 4.05
 print("The type of c is:",type(c)) # type of c is float
 
+<br>
+#Input in Python
+
+name=input("enter your name:")
+print("Welcome",name)
+
+
+name=input("enter your age:")
+print("Your age is:",name)
+
+value=input("enter your value:")
+print(type(value))
+
+value=int(input("enter your value:"))
+print(type(value))
+
+value=float(input("enter your value:"))
+print(type(value))
+
+name=input("enter your name:")
+age=int(input("enter your age:"))
+marks=float(input("enter your marks:"))
+
+print("Welcome",name)
+print("age is :",age)
+print("your marks:",marks)
+print(type(name))
+print(type(age))
+print(type(marks))
+
+
+
 
 
