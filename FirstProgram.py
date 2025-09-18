51f72e45bf053fb80c268bc845fde4a6513beec3
Aniket Yadav <anikety7905@gmail.com>
2025-09-18 09:38:54 +0530

    Update FirstProgram.py

--- FirstProgram.py
+++ FirstProgram.py
@@ -119,3 +119,15 @@ print("The value of c is:",c)# c is float because b is float, so a is converted 
 print("The type of a is:",type(a)) # type of a is int
 print("The type of c is:",type(c)) # type of c is float
 
+<br>
+ # Explicit Type Conversion
+a="5"
+b=4.05
+a=int(a) # converting string to int,because a is string
+print("The type of a is:",type(a)) # type of a is int
+c=a+b
+print("The value of c is:",c) # c is float because b is float, so a is converted to float, a = 5.0 and b = 4.05
+print("The type of c is:",type(c)) # type of c is float
+
+
+
