d5f1c0a3a782cd3155a3fa323fb2e3e29ab5fae6
Aniket Yadav <anikety7905@gmail.com>
2025-09-19 23:19:09 +0530

    Update FirstProgram.py

--- FirstProgram.py
+++ FirstProgram.py
@@ -190,6 +190,13 @@ b=int (input("enter your number b:"))
 print("a is greater than or equal to b:",a>=b)
 
 
+# String and Conditional statements.
+Str1="my name is aniket yadav "
+Str2='this is aniket yadav'
+Str3="""" my name is banku """
+
+
+
 
 
 
