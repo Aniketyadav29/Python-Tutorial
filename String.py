2a05d12f8c835c5107bef3d04570e2d473318429
Aniket Yadav <anikety7905@gmail.com>
2025-09-22 22:48:35 +0530

    Update String.py

--- String.py
+++ String.py
@@ -7,3 +7,14 @@ Str3="""" my name is banku """
 
 #Why it is used:-
 "this is apnacillege's tutorial"
+
+#Escape Sequence Character :-
+
+ Str4= "this is string.\n we are using it in python."
+ print(Str4)
+
+# Basic Operation on String
+
+#concatination
+
+print(Str1+Str2)
