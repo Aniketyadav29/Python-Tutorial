a65cf3881fdb1643f1b7ad6299a1b6da2cd9b003
Aniket Yadav <anikety7905@gmail.com>
2025-09-27 14:31:36 +0530

    Update 07- Conditional statements.py

--- 07- Conditional statements.py
+++ 07- Conditional statements.py
@@ -6,3 +6,14 @@ age=23
 if(age>=18):
     print("you are eligible for vote" )
     print("I am always executed")
+
+# if elif statement
+light="red"
+if (light=="red"):
+     print("you should stop")
+elif (light=="yellow"):
+     print("you should get ready")
+elif (light=="green"):
+     print("you can go")
+   
+print("end of the program")
