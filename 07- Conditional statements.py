fc36a480993f697a365160dbf4e59d6df551f9bb
Aniket Yadav <anikety7905@gmail.com>
2025-09-27 14:27:56 +0530

    Update and rename Conditional statements.py to 07- Conditional statements.py

---
+++ 07- Conditional statements.py
@@ -0,0 +1,8 @@
+# Conditional Statements in pyton:-
+
+# if statement
+
+age=23
+if(age>=18):
+    print("you are eligible for vote" )
+    print("I am always executed")
--- Conditional statements.py
+++
@@ -1,8 +0,0 @@
-# Conditional Statements in pyton:-
-
-# if statement
-
-age=23
- if (age>=18):
-     print("you are eligible for vote" )
-     print("I am always executed")
