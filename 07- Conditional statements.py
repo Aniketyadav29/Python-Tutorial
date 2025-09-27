56f12eb6eaf267ee283fb4b6e453f62bb927e894
Aniket Yadav <anikety7905@gmail.com>
2025-09-27 14:33:40 +0530

    Update 07- Conditional statements.py

--- 07- Conditional statements.py
+++ 07- Conditional statements.py
@@ -17,3 +17,30 @@ elif (light=="green"):
      print("you can go")
    
 print("end of the program")
+
+# difference between if and elif:-
+
+#if statement:- if statement is used to execute a block of code if a specified condition is true. if the condition is false, the block of code is skipped.
+num=5
+if (num>2):
+    print("num is greater than 2")
+if (num>4):
+    print("num is greater than 4")
+
+# elif statement:- elif statement is used to execute a block of code if the previous condition is false and the specified condition is true. if the condition is false, the block of code is skipped. 
+num=5
+if (num>2):
+    print("num is greater than 2")
+elif (num>4):
+    print("num is greater than 4") 
+
+#else statement:- else statement is used to execute a block of code if the previous condition is false. if the condition is true, the block of code is skipped.
+light="pink"
+if (light=="red"):
+    print("you should stop") # indented block
+elif (light=="yellow"):
+    print("you should get ready") #indentation block
+elif (light=="green"):
+    print("you can go") 
+else:
+    print("invalid color") # indented block 
