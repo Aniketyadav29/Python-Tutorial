22e05a295c33726a4a0d0b6b58751bf36a7371e7
Aniket Yadav <anikety7905@gmail.com>
2025-10-01 15:22:32 +0530

    Update 08 - List and Tuple.py

--- 08 - List and Tuple.py
+++ 08 - List and Tuple.py
@@ -11,3 +11,16 @@ print(Marks[2])
 Student=["Aniket",56,23,"Anupam Lata Yadav"]
 print(Student)
 print(len(Student))
+
+# List is mutable in python:-
+
+Student[0]="Anshu"
+print(Student)
+
+# List Slicing:-
+Mark=[23,56.3,56,78,90]
+print(Mark[1])
+print(Mark[1:4])
+print((Mark[:4]))
+print(Mark[1:])
+print(Marks[-3:-1]) 
