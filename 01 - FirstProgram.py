988bc947c0574557d2e406db011827040a7a9c68
Aniket Yadav <anikety7905@gmail.com>
2025-10-06 23:37:40 +0530

    Update 01 - FirstProgram.py

--- 01 - FirstProgram.py
+++ 01 - FirstProgram.py
@@ -398,8 +398,34 @@ print((Mark[:4]))
 print(Mark[1:])
 print(Mark[-3:-1]) 
 
+# List Methods:-
+List=[3,1,2]
+List.sort() # it sorts the list in ascending order
+print(List)
+List.reverse() # it reverses the list
+print(List)
+List.append(4) # it adds an element at the end of the list
+print(List)
+List.insert(2,5) # it adds an element at the specified index
+print(List)
+List.remove(5) # it removes the specified element
+print(List)
+
+# List Methods:-
+list=[1,2,3,3,5]
+list.remove(3) # it removes the first occurrence of the specified element
+print(list) 
+
+list.pop() # it removes the last element of the list
+print(list)
+
+list.append(6) # it adds an element at the end of the list
+print(list)
+
+
 
  
+
 
 
 
