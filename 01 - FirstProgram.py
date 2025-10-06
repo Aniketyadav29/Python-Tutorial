893042cd5a801530d31bc8a45d6899690bbf242d
Aniket Yadav <anikety7905@gmail.com>
2025-10-06 23:35:55 +0530

    Update 01 - FirstProgram.py

--- 01 - FirstProgram.py
+++ 01 - FirstProgram.py
@@ -385,9 +385,22 @@ Student=["Aniket",56,23,"Anupam Lata Yadav"]
 print(Student)
 print(len(Student))
 
+# List is mutable in python:-
+
+Student[0]="Anshu"
+print(Student)
+
+# List Slicing:-
+Mark=[23,56.3,56,78,90]
+print(Mark[1])
+print(Mark[1:4])
+print((Mark[:4]))
+print(Mark[1:])
+print(Mark[-3:-1]) 
 
 
  
+
 
 
 
