1a2cc40551a8dfb9153bf6e30f06801cf6d7f29f
Aniket Yadav <anikety7905@gmail.com>
2025-10-06 23:39:23 +0530

    Update 01 - FirstProgram.py

--- 01 - FirstProgram.py
+++ 01 - FirstProgram.py
@@ -422,9 +422,64 @@ print(list)
 list.append(6) # it adds an element at the end of the list
 print(list)
 
+#Tuple:- Tuple is immutable in python. it means we cannot change the elements of the tuple.it's a built-in data type in python.
+
+tup=(1,2,3,4,5)
+print(type(tup))
+print(tup[0])
+print(tup[1:4])
+print(tup[-3:-1])
+tup[0]=6 # it will give an error because tuple is immutable
+
+tup1=(1)
+print(type(tup1)) # it will give type 'int' because it's not a tuple, it's an integer
+tup2=("Aniket")
+print(type(tup2)) # it will give type 'str' because it's not a tuple, it's a string
+tup3=(1,2,3,4,5)
+print(type(tup3))# it will give type 'tuple' because it's a tuple
+
+# Tuple Methods:-
+tup=(1,2,3,4,5,3)
+print(tup.count(3)) # it counts the number of occurrences of the specified element
+print(tup.index(4)) # it returns the index of the first occurrence of the specified element
+print(tup.sort()) # it will give an error because tuple is immutable
+print(tup.reverse()) # it will give an error because tuple is immutable
+
+# practice:-
+# Question 1:- WAP to ask user to enter his favorite 5 movies and store them in a list.
+movies=[]
+for i in range(5):
+    movie=input("Enter your favorite movie:")
+    movies.append(movie)
+print("Your favorite movies are:",movies)
+# Methos :- 2
+movies=[]
+mov1=input("Enter your favorite movie1:")
+mov2=input("Enter your favorite movie2:")
+mov3=input("Enter your favorite movie3:")
+mov4=input("Enter your favorite movie4:")
+mov5=input("Enter your favorite movie5:")
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+movies.append(mov4)
+movies.append(mov5)
+print("Your favorite movies are:",movies)
+
+# Question 2:- WAP a program to check palindrome or not.
+list1=[1,2,1]
+list2=[1,2,3]
+copy_list1=list1.copy()
+list1.reverse()
+if (list1==copy_list1):
+    print("The list is palindrome")
+else:
+    print("The list is not palindrome")
+
 
 
  
+
 
 
 
