19e93b9b4ea37d4931b689e9eec817c825257a17
Aniket Yadav <anikety7905@gmail.com>
2025-10-06 23:35:00 +0530

    Update 01 - FirstProgram.py

--- 01 - FirstProgram.py
+++ 01 - FirstProgram.py
@@ -372,8 +372,23 @@ if(num%7==0):
 else:
     print("The Number is not Multiple by 7")
 
+# List:-
+
+Marks=[23,56.3,56,78,90]
+print(Marks)
+print(type(Marks))
+print(Marks[0])
+print(Marks[1])
+print(Marks[2])
+
+Student=["Aniket",56,23,"Anupam Lata Yadav"]
+print(Student)
+print(len(Student))
+
+
 
  
+
 
 
 
