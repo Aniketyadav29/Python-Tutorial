ef9b53fbc1b2b6ade81304073a290b2474d075e9
Aniket Yadav <anikety7905@gmail.com>
2025-10-08 23:02:21 +0530

    Update 09 - Dictionary and Sets.py

--- 09 - Dictionary and Sets.py
+++ 09 - Dictionary and Sets.py
@@ -3,6 +3,15 @@
 # Dictionary :- Dictionary are used to store data value in keys ; value = pair.
 # they are unorderd ,Mutable,can't accept duplicate keys.
 
+info ={
+    "key" : "value",
+    "Name":"Aniket Yadav",
+    "Age":"26",
+    "Section":"2C",
+    "Subject":"Math,Science,Java,Chemistry,Python",
+    "Topic":"Dictionary and Sets"
+}
+
 
     
 
