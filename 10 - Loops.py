9c98cb84815e3d58e06afe1d0d3acb1076e6f01d
Aniket Yadav <anikety7905@gmail.com>
2025-10-22 22:13:56 +0530

    Update 10 - Loops.py

--- 10 - Loops.py
+++ 10 - Loops.py
@@ -5,3 +5,32 @@
 
 # While loop :-
  # While Condition :- 
+# Example No.1:- 
+count=0 # Iterator Initialization.
+while (count<5): # Stopping Condition.
+    print("hello world")
+    count+=1 # Iterator Update.
+print("loop ended")
+
+# secomd example:-
+i=1  # Iterator Initialization.
+while (i<=10): # Stopping Condition.
+    print("hello world",i)
+    i+=1 # Iteration Update.
+print("Loop Ended")
+
+# Example No.4:- print reverse numbers from 10 to 1.
+i=10
+while(i>=1): # Stopping Condition.
+    print(i)
+    i-=1
+print("loop ended")
+
+# Example No.5:- print number from 1 to 10.
+i=1
+while(i<=10): # Stopping Condition.
+    print(i)
+    i+=1
+    print("loop ended")
+
+
