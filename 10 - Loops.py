d8e89bd7a5831d159caa651dc84c4870d4b07bd7
Aniket Yadav <anikety7905@gmail.com>
2025-10-26 16:03:54 +0530

    Update 10 - Loops.py

--- 10 - Loops.py
+++ 10 - Loops.py
@@ -234,6 +234,15 @@ for i in range(1,n+1): # Iteration Variable.
     factorial=factorial*i # Print Statement.  
 print("The factorial of the number is:",factorial)
 
+# using while loop
+n=int(input("Enter your number:"))
+factorial=1
+i=1
+while(i<=n): # Stopping Condition.
+    factorial=factorial*i # Print Statement.
+    i+=1
+
+
 
 
 
