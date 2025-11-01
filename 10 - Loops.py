5b2d42358a4b9cd2272b0450ace38e99f98002d3
Aniket Yadav <anikety7905@gmail.com>
2025-11-01 22:57:44 +0530

    Update 10 - Loops.py

--- 10 - Loops.py
+++ 10 - Loops.py
@@ -1,8 +1,5 @@
 # Loops:- Loops are used to execute a block of code repeatedly until a certain condition is met.
 
-# or
- # loops are used to repeate the certain instruction.
-
 # While loop :-
  # While Condition :- 
 # Example No.1:- 
