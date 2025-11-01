f3521b0f86b5d4f93e41d7fefdff41e59873b947
Aniket Yadav <anikety7905@gmail.com>
2025-11-01 23:03:13 +0530

    Update 02 - Comments.py

--- 02 - Comments.py
+++ 02 - Comments.py
@@ -1,4 +1,4 @@
-# comment :-
+# comment :- In Python, comments are notes you write in your code to explain what it does — they are ignored by the Python interpreter and do not affect program execution.
 # this is a single line comment
 '''
 this is a multi line comment
