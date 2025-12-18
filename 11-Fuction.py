bae832273077a916c50106138415dece16a30318
Aniket Yadav <anikety7905@gmail.com>
2025-12-18 22:03:57 +0530

    Update 11-Fuction.py

--- 11-Fuction.py
+++ 11-Fuction.py
@@ -1,1 +1,9 @@
 
+# Function:- Function is a block of code which only runs when it is called. You can pass data, known as parameters, into a function. A function can return data as a result.
+
+# a=4
+# b=3
+
+# sum=a+b 
+# print(sum) #print("This is addition function")
+
