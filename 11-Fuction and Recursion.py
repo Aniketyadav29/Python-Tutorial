1d981befa9166c16e5794a06a7793a6739bb3a26
Aniket Yadav <anikety7905@gmail.com>
2025-12-21 12:46:14 +0530

    Add recursion explanation comments
    Added comments explaining recursion concept.

--- 11-Fuction and Recursion.py
+++ 11-Fuction and Recursion.py
@@ -112,6 +112,9 @@ def convert_usd_to_inr(usd):
     return inr
 convert_usd_to_inr(100)
 
+# Recursion:- Recursion is a process in which a function calls itself directly or indirectly. 
+#The main idea of recursion is to break a complex problem into smaller sub-problems that are easier to solve.
+
 
 
 
