e68eff6ac32b3fa4671d5e5864c2cb117205efcc
Aniket Yadav <anikety7905@gmail.com>
2026-03-07 22:09:34 +0530

    Implement star pattern printing program
    Added a program to print a star pattern based on user input.

--- 10 - Loops/Practice.py
+++ 10 - Loops/Practice.py
@@ -115,6 +115,19 @@ for i in range(1,n+1):
     product=product*i
 print(f"The factorial of {n} is {product}")
 
+
+# Question.7:- Write a program to print star pattern
+For n=3
+ *
+***
+***** 
+
+n=int(input("Enter your Number."))
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    print("*"*(2*i-1),end="")
+    print("")
+
 # ==========================================================
 # Practice Session Complete!
 # ==========================================================
