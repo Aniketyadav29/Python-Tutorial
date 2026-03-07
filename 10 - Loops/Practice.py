34f86f355539d52aaa37b160f33fdcaa86c45c06
Aniket Yadav <anikety7905@gmail.com>
2026-03-07 22:10:31 +0530

    Implement star pattern printing for user input
    Added a program to print a star pattern for a given number n.

--- 10 - Loops/Practice.py
+++ 10 - Loops/Practice.py
@@ -134,6 +134,20 @@ for i in range(1,n+1):
     print("*"*i,end="")
     print("")
 
+ # Question.9:-  write a program to print the following star pattern.
+ # * * *
+ # *   *
+ # * * * for n=3
+n=int(input("Enter Your Number."))
+for i in range(1,n+1):
+    if (i==1 or i==n ):
+       print("*"*n)
+    else:
+        print("*",end="")
+        print(" "*(n-2),end="")
+        print("*",end="")
+        print("")
+
 # ==========================================================
 # Practice Session Complete!
 # ==========================================================
