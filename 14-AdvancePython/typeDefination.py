46e5569238b619a2e112320d4b19bac9e6284b13
Aniket Yadav <anikety7905@gmail.com>
2026-03-26 23:48:05 +0530

    Implement type definitions for function and collections
    Added type annotations for function and data structures.

--- 14-AdvancePython/typeDefination.py
+++ 14-AdvancePython/typeDefination.py
@@ -1,1 +1,31 @@
+def add(a: int, b: int) -> int:
+    return a + b
 
+print(add("a", "b"))  
+
+# List:-
+
+from typing import List
+
+numbers: List[int] = [1, 2, 3]
+print(numbers)
+
+#Tuples:-
+
+from typing import Tuple
+
+point: Tuple[int, int] = (2, 3)
+print(point)
+
+
+# Dictionary:-
+from typing import Dict
+
+data: Dict[str, int] = {"age": 21}
+print(data)
+
+# Set :-
+from typing import Set
+
+s: Set[int] = {1, 2, 3}
+print(s)
