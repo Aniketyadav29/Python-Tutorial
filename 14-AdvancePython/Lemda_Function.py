acdcbcf2734e72ad8ea887e0225ef9ac0f167ee5
Aniket Yadav <anikety7905@gmail.com>
2026-03-29 23:39:35 +0530

    Update Lemda_Function.py

--- 14-AdvancePython/Lemda_Function.py
+++ 14-AdvancePython/Lemda_Function.py
@@ -1,1 +1,18 @@
+# Normal Method:-
+def square():
+    n=int(input("Enter Your Number."))
+    return n*n
+print(square())
+
+# Lemda Function:-
+
+square=lambda x:x*x
+print(square(int(input("Enter Your Number ."))))
+
+# Map:-
+
+l=[1,2,3,4,5]
+square=lambda x:x*x
+sqlist=map(square,l)
+print(list(sqlist))
 
