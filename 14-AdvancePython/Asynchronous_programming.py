fa79bb29ca2f77c154dd818051a4de03f457a2f5
Aniket Yadav <anikety7905@gmail.com>
2026-04-01 22:50:26 +0530

    Update Asynchronous_programming.py

--- 14-AdvancePython/Asynchronous_programming.py
+++ 14-AdvancePython/Asynchronous_programming.py
@@ -1,1 +1,10 @@
-.
+// Asynchronous Programming//
+import time
+
+def task(name):
+    print(f"Start {name}")
+    time.sleep(2)   # blocks execution
+    print(f"End {name}")
+
+task("A")
+task("B")
