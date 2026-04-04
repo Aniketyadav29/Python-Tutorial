0683064bb3ce89a4ff5aa660859cc4400b091063
Aniket Yadav <anikety7905@gmail.com>
2026-04-04 23:34:28 +0530

    Update Security.py

--- 14-AdvancePython/Security.py
+++ 14-AdvancePython/Security.py
@@ -12,7 +12,18 @@ hashed = hashlib.sha256(password.encode()).hexdigest()
 
 print("Hashed password:", hashed)
 
-# Password verify code :- 
+# Password verify code :-
 
 def verify_password(input_password, stored_hash):
     return hashlib.sha256(input_password.encode()).hexdigest() == stored_hash
+
+
+# SQL Injection Attack:-
+username = input("Enter username: ")
+
+query = f"SELECT * FROM users WHERE username = '{username}'"
+
+
+
+
+
