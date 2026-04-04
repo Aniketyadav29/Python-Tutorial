c33788894f02504fc4883385015a2a41e9655879
Aniket Yadav <anikety7905@gmail.com>
2026-04-04 23:31:49 +0530

    Update Security.py

--- 14-AdvancePython/Security.py
+++ 14-AdvancePython/Security.py
@@ -1,1 +1,13 @@
-.
+password = "mypassword123" # wrong method .
+
+
+# Hashing using Hashlib
+
+import hashlib
+
+password = "mypassword123"
+
+# Convert to hash
+hashed = hashlib.sha256(password.encode()).hexdigest()
+
+print("Hashed password:", hashed)
