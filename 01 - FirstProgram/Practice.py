5563c6f97260f32d54fe13ba3245e7b19f680f4c
Aniket Yadav <anikety7905@gmail.com>
2026-04-14 22:21:36 +0530

    Update Practice.py

--- 01 - FirstProgram/Practice.py
+++ 01 - FirstProgram/Practice.py
@@ -410,4 +410,42 @@ d2 = {"b": 3, "c": 4}
 d1.update(d2)
 print(d1)
 
+# Wallrus Method :-
+
+# Normal Method :-
+n = len("Hello")
+if n > 3:
+    print(n)
+
+# Walrus Method :-
+
+
+if(n:=len("Hello"))>3:
+    print(n)
+
+
+data = input("Enter: ")
+while (data != "exit"):
+    print(data)
+    # data = input("Enter: ")
+
+# Walrus Methid:
+
+while(data:=input("Enter:")) != "exit":
+    print(data)
+
+# walrus Operator:-
+
+numbers = [1, 2, 3, 4, 5]
+
+if (n := len(numbers)) > 3:
+    print(f"List is long: {n}")
+
+# normal Way:-
+
+number=[1,2,3,4,5]
+m=len(number)
+if(m>3):
+    print (f"list is long {m}")
+
 
