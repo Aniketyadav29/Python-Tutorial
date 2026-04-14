5e96d0b0092bad0fb1b4b2aaa622b47f70556036
Aniket Yadav <anikety7905@gmail.com>
2026-04-14 22:14:41 +0530

    Update Practice.py

--- 01 - FirstProgram/Practice.py
+++ 01 - FirstProgram/Practice.py
@@ -324,3 +324,49 @@ class Student:
         self.name = name
 s1 = Student("Aniket Yadav")
 print(s1.name)
+
+# MathCase:-
+n=("Aniket")
+match n:
+    case 1:
+        print("Yes")
+    case 2:
+        print("No")
+
+    case _:
+    
+        print("Other") # if case 1 and case 2 doesn't match with variable's valur , default it prints the value .
+
+
+#
+
+x = 2
+
+match x:
+    case 1:
+        print("One")
+    case 2:
+        print("Two")
+    case _:
+        print("Other")
+
+# Multiple values in one case :-
+
+x = 3
+
+match x:
+    case 1 | 2:
+        print("One or Two")
+    case 3 | 4:
+        print("Three or Four")  
+
+# Variables:- 
+
+data = ("Ani", 21)
+
+match data:
+    case (name,age):
+        print(name, age)   
+    case _:
+        print("none")
+
