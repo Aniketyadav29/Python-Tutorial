cd9f890c2e5f19f447658ef77241a5c042f7a031
Aniket Yadav <anikety7905@gmail.com>
2026-04-17 23:24:30 +0530

    Add map example using lambda function for squaring

--- 01 - FirstProgram/Practice.py
+++ 01 - FirstProgram/Practice.py
@@ -468,6 +468,13 @@ print(square())
 square=lambda x:x*x
 print(square(int(input("Enter Your Number ."))))
 
+# Map:-
+
+l=[1,2,3,4,5]
+square=lambda x:x*x
+sqlist=map(square,l)
+print(list(sqlist))
+
 
 
 
