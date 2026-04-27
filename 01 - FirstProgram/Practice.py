8681438c2a7488b191e5090deeeff2e1eea04f65
Aniket Yadav <anikety7905@gmail.com>
2026-04-27 23:37:07 +0530

    Update Practice.py

--- 01 - FirstProgram/Practice.py
+++ 01 - FirstProgram/Practice.py
@@ -529,6 +529,24 @@ else:
     else:
         print("somthing Went Wrong.")
 
+# Asychronous programming :-
+# // Normal Method Programming//
+import time
+
+def task(name):
+    print(f"Start {name}")
+    time.sleep(2)   # blocks execution
+    print(f"End {name}")
+
+task("A")
+task("B")
+
+Output:-
+Start A
+End A
+Start B
+End B
+
 
 
 
